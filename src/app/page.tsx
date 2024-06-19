9b11c327fbb3094ff0fca70353f84365525ef429
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-06-18 21:56:57 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const session = useSession
+  const createFile = useMutation(api.files.createFile);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -18,6 +20,15 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      <Button
+        onClick={() => {
+          createFile({
+            name: 'Hello Worldddd'
+          })
+        }}>
+        Click Me
+      </Button>
     </main>
   );
 }
