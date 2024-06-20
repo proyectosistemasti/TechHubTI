5fe51059de5d536a7923da215fcb4f632c3cf540
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-06-20 15:58:37 -0500

    update

--- convex/files.ts
+++ convex/files.ts
@@ -4,6 +4,7 @@ import { mutation, query } from "./_generated/server";
 export const createFile = mutation({
   args: {
     name: v.string(),
+    orgId: v.string(),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
@@ -13,17 +14,22 @@ export const createFile = mutation({
 
     await ctx.db.insert("files", {
       name: args.name,
+      orgId: args.orgId,
     });
   },
 });
 
 export const getFiles = query({
-  args: {},
+  args: {
+    orgId: v.string(),
+  },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
-    if(!identity) {
+    if (!identity) {
       return [];
     }
-    return ctx.db.query("files").collect();
+    return ctx.db
+      .query("files")
+      .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId)).collect();
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,33 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignOutButton, useOrganization } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const {organization} = useOrganization();
-  const files = useQuery(api.files.getFiles)
+  const organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : 'skip');
   const createFile = useMutation(api.files.createFile);
 
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <SignedIn>
-        <SignOutButton>
-          <Button>Sign Out</Button>
-        </SignOutButton>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>
       })}
 
       <Button
         onClick={() => {
+          if (!orgId) return;
           createFile({
-            name: 'Hello Worldddd'
+            name: 'Hello Worldddd',
+            orgId,
           })
         }}>
         Click Me
