3c40e69a74b9406125f0d06722603f5a756f2379
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-07-19 10:14:01 -0500

    test

--- src/app/dashboard/side-nav.tsx
+++ src/app/dashboard/side-nav.tsx
@@ -34,7 +34,7 @@ export function SideNav() {
             <Icon
               className={clsx(
                 "transition-colors duration-300",
-                { [colorClass]: pathname.includes(href), [hoverClass]: true }
+                { [colorClass]: pathname.includes(href), [hoverClass]: false }
               )}
             />
             {label}
