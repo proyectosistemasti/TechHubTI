bde39ba934d4b6de73eb9ca37d5e5ecd2f9027c6
AlejandroRomero17 <gonzalez.romero.alejandroo@gmail.com>
2024-07-19 13:09:09 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ import Pricing from "@/_components/Pricing";
 import Testimonials from "@/_components/Testimonials";
 import Video from "@/_components/Video";
 import { Inter } from "@next/font/google";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  redirect('/home');
   return (
     <>
       <ScrollUp />
