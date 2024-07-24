44bb5c99137711a363b7a458a550d38bed165583
proyectosistemasti <proyectosistemasti@gmail.com>
2024-07-24 11:10:36 -0500

    update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://stirring-bass-93.clerk.accounts.dev",
+      domain: "https://fluent-ferret-24.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
